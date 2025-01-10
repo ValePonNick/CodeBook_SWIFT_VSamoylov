ffe2aa126f9821e1245db8c3cc5e7bf412d3fb42
valery ponomareva <valeryponomareva378he@gmail.com>
2025-01-10 10:38:52 +0700

    CodeBook_SWIFT_VSamoylov
     вывела кортеж / элементы кортежа на консоль

--- tuples.playground/Contents.swift
+++ tuples.playground/Contents.swift
@@ -1,4 +1,39 @@
 import UIKit
 
-// TUPLES. КОРТЕЖИ.
+// 5. TUPLES. КОРТЕЖИ.
 
+// 5.1 создать кортеж с 3-5 значениями
+let tupleFruits = (fruit: "Apple", count: 12, isFresh: true)
+
+// вывести на консоль 3-мя способами:
+// 5.1.1 вывод ЭЛЕМЕНТОВ кортежа по индексам
+print(tupleFruits.0)
+print(tupleFruits.1)
+print(tupleFruits.0, tupleFruits.1, tupleFruits.2)
+// или так:
+print("fruit: \(tupleFruits.0)")
+print("count: \(tupleFruits.1)")
+print("fruit: \(tupleFruits.0), count: \(tupleFruits.1), isFresh: \(tupleFruits.2)")
+
+tupleFruits.fruit
+print(tupleFruits.fruit)
+tupleFruits.count
+print(tupleFruits.count)
+tupleFruits.isFresh
+print(tupleFruits.isFresh)
+// 5.1.2 вывод кортежа НАПРЯМУЮ
+print(tupleFruits)
+// 5.1.3 вывод ЭЛЕМЕНТОВ кортежа с именованными полями
+print(tupleFruits.fruit)
+print(tupleFruits.count)
+print(tupleFruits.fruit, tupleFruits.count, tupleFruits.isFresh)
+// или так:
+print("\(tupleFruits.fruit)")
+print("\(tupleFruits.count)")
+print("\(tupleFruits.fruit), \(tupleFruits.count), \(tupleFruits.isFresh)")
+// 5.1.4 деконструкция кортежа
+let (fruit, count, isFresh) = ("Apple", 12, true)
+print(fruit)
+print(count)
+print(isFresh)
+// комментарий: решено
