022287684959b7946ec873611643a94a57222481
valery ponomareva <valeryponomareva378he@gmail.com>
2025-01-11 22:58:41 +0700

    CodeBook_SWIFT_VSamoylov

--- tuplesadvancedLevel.playground/Contents.swift
+++ tuplesadvancedLevel.playground/Contents.swift
@@ -1,5 +1,30 @@
 import UIKit
 
-var greeting = "Hello, playground"
 // TUPLES: ADVANCED LEVEL
+// 1. создай 10 кортежей-айтишников, каждый с 4-мя параметрами: имя, возраст, опыт, специальность. НЕ задавай НИКАКИХ данных.
+// объявим кортеж с явно заданнным типом
+var techieLiam: (name: String, age: Int, experiencs: String, specialty: String)
+var techieStive: (name: String, age: Int, experiencs: String, specialty: String)
+var techieAnn: (name: String, age: Int, experiencs: String, specialty: String)
+var techieJames: (name: String, age: Int, experiencs: String, specialty: String)
+var techieOliver: (name: String, age: Int, experiencs: String, specialty: String)
+var techieDavid: (name: String, age: Int, experiencs: String, specialty: String)
+var techieLeo: (name: String, age: Int, experiencs: String, specialty: String)
+var techieRobert: (name: String, age: Int, experiencs: String, specialty: String)
+var techieHarry: (name: String, age: Int, experiencs: String, specialty: String)
+var techieBenedict: (name: String, age: Int, experiencs: String, specialty: String)
+
+// всем задать имя, 3-м из них - возраст (> 23), 4-м - опыт, 2-м - спец-ть
+techieLiam = ("Liam", 0, "", "")
+techieStive = ("Stive", 0, "", "")
+techieAnn = ("Ann", 25, "", "")
+techieJames = ("James", 32, "", "")
+techieOliver = ("Oliver", 21, "", "")
+techieDavid = ("David", 0, "middle", "")
+techieLeo = ("Leo", 0, "senior", "")
+techieRobert = ("Robert", 0, "junior", "")
+techieHarry = ("Harry", 0, "junior", "iOS")
+techieBenedict = ("Benedict", 0, "", "iOS")
+
+/* 3. используя проверки if/else, выведи сообщение - логика: если возраст больше 23б есть опыт и специальность - это iOS - в консоль выведи имя и сообщение - принят на работу */
 
