15c332d7dedb6e37df8548c5ef3595b9a227b9d9
vale <Vale.ponick@gmail.com>
2025-01-13 08:44:29 +0700

    CodeBook_SWIFT_VSamoylov

--- tuplesadvancedLevel.playground/Contents.swift
+++ tuplesadvancedLevel.playground/Contents.swift
@@ -78,20 +78,54 @@ print(arrayYoungTechie)
 // выведи измененный масссив
 print(arrayYoungTechie)
 // Найди в массиве max значение и удали его.
+// вар. 1:
 // шаг 1: находим max элемент
 var sortedArrayYoungTechie = arrayYoungTechie.sorted()
 // шаг 2: находим ИНДЕКС max элемента
 if let maxElement = sortedArrayYoungTechie.max() {
-    if let index = arrayYoungTechie.firstIndex(of: maxElement) {
+    if let index = sortedArrayYoungTechie.firstIndex(of: maxElement) {
         // удалим max элемент
-        arrayYoungTechie.remove(at: index)
+        sortedArrayYoungTechie.remove(at: index)
     }
 }
 // выведи измененный масссив
-print(arrayYoungTechie)
+print(sortedArrayYoungTechie)
+/* комментарий:
+ 1. находим max элемент: метод max() -> опциональное значение => Нужно использовать конструкцию 'if let' для БЕЗОПАСНОГО извлечения
+ 2. находим индекс: метод 'firstIndex(of:)'  -> индекс ПЕРВОГО вхождения этого элемента в массив
+ 3. удаляем элемент:  метод 'remove(at:)' удаляет элемент по указанному индексу
+ 4. выводим рез-т: ПОСЛЕ удаления max элеента выводим ИЗМЕНЕННЫЙ массив */
+
+// вар. 2
+// если нужно удалить ВСЕ max элеенты, используй метод 'filter:()'
+let maxElrment = sortedArrayYoungTechie.max()
+sortedArrayYoungTechie = sortedArrayYoungTechie.filter { $0 != maxElrment }
+// выведи измененный масссив
+print(sortedArrayYoungTechie)
 
 // 5. создай массив и в проверку из п.3 добавь условие: если специальность не iOS - добавить в созданный массив
-
-
+var arrayNotIOS: [String] = []
+if techieAnn.specialty != "iOS" {
+    arrayNotIOS.append(techieAnn.name)
+}
+if techieDavid.specialty != "iOS" {
+    arrayNotIOS.append(techieDavid.name)
+}
+if techieHarry.specialty != "iOS" {
+    arrayNotIOS.append(techieHarry.name)
+}
+if techieBenedict.specialty != "iOS" {
+    arrayNotIOS.append(techieBenedict.name)
+}
+if techieLeo.specialty != "iOS" {
+    arrayNotIOS.append(techieLeo.name)
+}
+if techieLiam.specialty != "iOS" {
+    arrayNotIOS.append(techieLiam.name)
+}
+if techieOliver.specialty != "iOS" {
+    arrayNotIOS.append(techieOliver.name)
+}
+print(arrayNotIOS)
 // проверим! поменяла почту на github desktop... получилось!!!
 
