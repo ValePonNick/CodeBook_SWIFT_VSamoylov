522d32837ede048ba995a34de32fd64470d16038
vale <Vale.ponick@gmail.com>
2025-01-14 23:32:01 +0700

    CodeBook_SWIFT_VSamoylov

--- flowControl_Loops_AdvancedLevel.playground/Contents.swift
+++ flowControl_Loops_AdvancedLevel.playground/Contents.swift
@@ -7,7 +7,7 @@ import UIKit
 
 // кортеж шахматных фигур
 let chessBoardRow = (
-    "Rook", "Knight", "Bishop", "Qeen", "King", "Bishop", "Knight", "Rook"
+    "Rook", "Knight", "Bishop", "Qeen", "King", "Bishop", "Knight", "Rook", "Pawn"
 )
 // использование 2-мерного МАССИВА для создания шахматной доски
 var chessboard: [[String]] = Array(repeating: Array(repeating: "Empty", count: 8), count: 8)
@@ -20,6 +20,22 @@ chessboard[0][4] = "King"
 chessboard[0][5] = "Bishop"
 chessboard[0][6] = "Knight"
 chessboard[0][7] = "Rook"
+chessboard[1][0] = "Pawn"
+chessboard[1][1] = "Pawn"
+chessboard[1][2] = "Pawn"
+chessboard[1][3] = "Pawn"
+chessboard[1][4] = "Pawn"
+chessboard[1][5] = "Pawn"
+chessboard[1][6] = "Pawn"
+chessboard[1][7] = "Pawn"
+chessboard[7][0] = "Pawn"
+chessboard[7][1] = "Pawn"
+chessboard[7][2] = "Pawn"
+chessboard[7][3] = "Pawn"
+chessboard[7][4] = "Pawn"
+chessboard[7][5] = "Pawn"
+chessboard[7][6] = "Pawn"
+chessboard[7][7] = "Pawn"
 
 // функция для вывода шахматной доски
     for row in chessboard {
