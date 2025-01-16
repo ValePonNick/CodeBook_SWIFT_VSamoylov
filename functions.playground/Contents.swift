2d35ba21dbddc307d0149c024bf757e30e8a2a9c
vale <Vale.ponick@gmail.com>
2025-01-16 13:11:18 +0700

    CodeBook_SWIFT_VSamoylov

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -14,15 +14,22 @@ let sortedNums = sortNumbers(number: unsortedNums)
 type(of: sortedNums)
 print(sortedNums)
 /* вторая - принимает массив String, меняет 1,2 и 3 элементы и сортирует по алфавиту. Затем распечатывает рез-т в консоль */
+/* NB: эта функция использует встроенный метод 'sorted()' для сортировки массива строк в лексикографическом порядке */
 func sortStrings(array: [String])  -> [String] {
     return array.sorted()
 }
 let strings = ["apple", "banana", "kivi", "limon"]
 let sortedStrings = sortStrings(array: strings)
 print(sortedStrings)
-/* NB: эта функция использует встроенный метод 'sorted()' для сортировки массива строк в лексикографическом порядке */
 
 /* третья - принимает ДВА масссива String и складывает их. Затем распечатывает рез-т в консоль */
+func twoArrays(_: [String], _: [String]) {
+    var sum = array1 + array2
+    print(sum)
+}
+let array1 = ["one", "two", "three", "four"]
+let array2 = ["apple", "limon", "cheese"]
+twoArrays(_: array1, _: array2)
 
 /*2. создать журнал для учителя, который будет принимать имя студента, профессию и оценку и записывает все это в массив. И внестите 10 студентов туда и распечатайте через цикл for - in */
 
