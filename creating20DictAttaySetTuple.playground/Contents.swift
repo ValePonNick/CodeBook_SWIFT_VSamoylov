96e55a839c7424e477fb05f26413265d75dbbeda
vale <Vale.ponick@gmail.com>
2025-02-13 23:04:11 +0700

    CodeBook_SWIFT_VSamoylov

--- creating20DictAttaySetTuple.playground/Contents.swift
+++ creating20DictAttaySetTuple.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
 // Создай по 20 видов коллекций + кортежей
+
+// REPEATING SINTAX: Arrays
+
+let arrayNames = ["Bob", "Joe", "Christie"]
+arrayNames[0] // доступ к элементу по ключу
+
+// REPEATING SINTAX: Dictionaries
+let dictNames = ["Bob": 12, "Joe": 21]
+dictNames.index(forKey:"Joe")
+
+// REPEATING SINTAX: Sets
+
+let setNames: Set<Int> = [12, 21, 32]
+
+// REPEATING SINTAX: Tuples
+
+let tupleNames = ("Joe", 112, "ID", "true")
