f33bcd64b2f6984fde99693d659474c3ae1a4471
vale <Vale.ponick@gmail.com>
2025-02-15 19:00:37 +0700

    CodeBook_SWIFT_VSamoylov

--- creating20DictAttaySetTuple.playground/Contents.swift
+++ creating20DictAttaySetTuple.playground/Contents.swift
@@ -29,10 +29,34 @@ var dictStudents = ["Roy": "ID = 112", "Joy": "ID = 112", "Key": "ID = 113"]
 dictStudents.removeValue(forKey: "Roy")
 print(dictStudents)
 
-                    
+var dictIT = ["John": "IT", "Sem": "History", "Andy": "IT", "Tom": "IT"]
+dictIT.removeValue(forKey: "John")
+dictIT.index(forKey: "Sem")
+print(dictIT)
+
+var dictNumbers = [1: 12.12, 2: 14.14, 3: 16.16, 4: 18.18]
+dictNumbers.values
+dictNumbers.keys
+
+var dictDouble = ["one": 123.123, "two": 124.124, "three": 125.125]
+dictDouble.count
+
 // REPEATING SINTAX: Sets
 
 let setNames: Set<Int> = [12, 21, 32]
+
+let setNums: Set<String> = ["one", "one","one", "one", "two", "two", "two", "three"]
+print(setNums) // ["one", "two", "three"]
+
+let setString: Set<String> = ["Tom", "Ann", "Sara", "Bob"]
+setString.count
+
+let setInts: Set<Int> = [5, 6, 7, 1, 2, 3, 4, 5, 6, 7]
+type(of: setInts)
+setInts.sorted() // [1, 2, 3, 4, 5, 6, 7]
+
+let setBool: Set<Bool> = [true, false, true, false]
+print(setBool.count) // 2
 
 // REPEATING SINTAX: Tuples
 
@@ -44,3 +68,7 @@ tupleClildren.self
 
 let tupleDates = (2025, 21.12, "дюжина", 21, true)
 tupleDates.4
+
+let tupleStudents = ("Sem", 21, "ID", true, "IT")
+tupleStudents.4 // "IT"
+
